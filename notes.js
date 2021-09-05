97460303c0c33ef15d33b4275317a2d197eda6eb
ivangayton <ivangayton@gmail.com>
2021-09-06 08:24:47 +0900

    figured out how to place external javascript properly

--- notes.js
+++ notes.js
@@ -1,18 +1,20 @@
-<script>
-let header = "Notes";
-let naturals = ["A","B","C","D","E","F","G"];
-let sharps = ["A&#x266F;","C&#x266F;","D&#x266F;","F&#x266F;","G&#x266F;"]
-let flats = ["D&#x266D;","E&#x266D;","G&#x266D;","A&#x266D;","B&#x266D;"]
 
-let allnotes = naturals.concat(sharps, flats)
-allnotes.sort(function(a, b){return 0.5 - Math.random()});
-
-let html = `<h2>${header}</h2><ul>`;
-
-for (const x of allnotes) {
-  html += `<li>${x}</li>`;
+function writeRandomNotes()
+{
+    header = "Notes";
+    let naturals = ["A","B","C","D","E","F","G"];
+    let sharps = ["A&#x266F;","C&#x266F;","D&#x266F;","F&#x266F;","G&#x266F;"]
+    let flats = ["D&#x266D;","E&#x266D;","G&#x266D;","A&#x266D;","B&#x266D;"]
+        
+    let allnotes = naturals.concat(sharps, flats)
+    allnotes.sort(function(a, b){return 0.5 - Math.random()});
+        
+    let html = `<h2>${header}</h2><ul>`;
+        
+    for (const x of allnotes) {
+      html += `<li>${x}</li>`;
+    }
+        
+    html += `</ul>`;
+    document.getElementById("displayNotes").innerHTML = html;
 }
-
-html += `</ul>`;
-document.getElementById("demo").innerHTML = html;
-</script>
